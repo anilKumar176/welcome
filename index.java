863d7a6af10bb4585e9eac44002131a2d713e517
sunil chavan <anilkumar@76>
2024-11-04 14:19:26 +0530

    add

---
+++ index.java
@@ -0,0 +1,10 @@
+public class index {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int swap=a;
+        a=b;
+        b=swap;
+        System.out.println(a+""+b);
+    }
+}
